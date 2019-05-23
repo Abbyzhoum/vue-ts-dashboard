bfd0c422bb151360910c71c2daf5983f67457ef9
abby <abby@welljoint.com`>
2019-05-23 14:11:59 +0800

    ts创建项目

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+
+
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+// export default{
+//   name: 'App'
+// }
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({})
+
+export default class App extends Vue {
+
+}
+</script>
+
+<style>
+html{
+  height: 100%;
+  background: #eee
+}
+</style>
+
